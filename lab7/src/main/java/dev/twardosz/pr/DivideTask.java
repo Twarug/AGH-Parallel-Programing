236e0f24d326dff54be79e3afa27c8ee231b4026
Twarug <patryk280502@gmail.com>
2024-11-17 13:14:29 +0100

    Lab7: Całka ForkJoinPool

--- lab7/src/main/java/dev/twardosz/pr/DivideTask.java
+++ lab7/src/main/java/dev/twardosz/pr/DivideTask.java
@@ -1,5 +1,7 @@
 package dev.twardosz.pr;
 
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 class DivideTask extends RecursiveTask<int[]> {
@@ -11,20 +13,30 @@ class DivideTask extends RecursiveTask<int[]> {
     }
 
     protected int[] compute() {
+        if (arrayToDivide.length <= 1) {
+            return arrayToDivide;
+        }
 
         // .......
 
-        DivideTask task1 = null; //new DivideTask(....);
-        DivideTask task2 = null; //new DivideTask(....);
+        int mid = arrayToDivide.length / 2;
+        int[] leftPart = new int[mid];
+        int[] rightPart = new int[arrayToDivide.length - mid];
 
-        // .......
+        System.arraycopy(arrayToDivide, 0, leftPart, 0, mid);
+        System.arraycopy(arrayToDivide, mid, rightPart, 0, arrayToDivide.length - mid);
+
+        // Tworzenie nowych zadań dla podtablic
+        DivideTask task1 = new DivideTask(leftPart);
+        DivideTask task2 = new DivideTask(rightPart);
+
+        invokeAll(task1, task2);
 
         //Wait for results from both tasks
         int[] tab1 = task1.join();
         int[] tab2 = task2.join();
 
         int[] scal_tab = new int[tab1.length + tab2.length];
-
         scal_tab(tab1, tab2, scal_tab);
 
         return scal_tab;
@@ -63,4 +75,18 @@ class DivideTask extends RecursiveTask<int[]> {
         }
     }
 
+    public static void main(String[] args) {
+        // Testowa tablica
+        int[] array = {38, 27, 43, 3, 9, 82, 10};
+        System.out.println("Przed sortowaniem: " + Arrays.toString(array));
+
+        // Utworzenie ForkJoinPool
+        ForkJoinPool pool = new ForkJoinPool();
+
+        // Rozpoczęcie zadania
+        DivideTask mainTask = new DivideTask(array);
+        int[] sortedArray = pool.invoke(mainTask);
+
+        System.out.println("Po sortowaniu: " + Arrays.toString(sortedArray));
+    }
 }
