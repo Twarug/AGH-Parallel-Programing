ab3ee1bd6b21b4d771d5a95533f4b190a3756dd1
Twarug <patryk@twardosz.dev>
2024-12-08 19:32:15 +0100

    Lab10: Mat_vec

--- lab10/mat_vec/mat_vec.c
+++ lab10/mat_vec/mat_vec.c
@@ -17,11 +17,10 @@ void mat_vec_row(double* a, double* x, double* y, int n)
 
 void mat_vec_row_row_decomp(double* a, double* x, double* y, int n)
 {
-  int i,j;
-
-  for(i=0;i<n;i++){
+  #pragma omp parallel for default(none) shared(a, x, y, n)
+  for(int i=0;i<n;i++){
     y[i]=0.0;
-    for(j=0;j<n;j++){
+    for(int j=0;j<n;j++){
       y[i]+=a[n*i+j]*x[j];
     }
   }  
@@ -29,11 +28,11 @@ void mat_vec_row_row_decomp(double* a, double* x, double* y, int n)
 
 void mat_vec_row_col_decomp(double* a, double* x, double* y, int n)
 {
-  int i,j;
-
-  for(i=0;i<n;i++){
+  #pragma omp parallel for default(none) shared(a, x, y, n)
+  for(int i=0;i<n;i++){
     y[i]=0.0;
-    for(j=0;j<n;j++){
+    #pragma omp parallel for reduction(+:y[i]) default(none) shared(a, x, n, i)
+    for(int j=0;j<n;j++){
       y[i]+=a[n*i+j]*x[j];
     }
   }  
@@ -54,25 +53,32 @@ void mat_vec_col(double* a, double* x, double* y, int n)
 
 void mat_vec_col_col_decomp(double* a, double* x, double* y, int n)
 {
+  for(int i=0;i<n;i++) y[i]=0.0;
   
-  int i,j;
+  #pragma omp parallel shared(a,x,y,n) default(none)
+  {
+    double* y_local = calloc(n, sizeof(double));
+    
+    #pragma omp for
+    for(int i=0;i<n;i++)
+      for(int j=0;j<n;j++)
+        y_local[i] += a[i + j * n] * x[j];
+  
+    #pragma omp critical
+    for(int i = 0; i < n; ++i) 
+      y[i] += y_local[i];
 
-  for(i=0;i<n;i++) y[i]=0.0;
-  for(j=0;j<n;j++){
-    for(i=0;i<n;i++){
-      y[i]+=a[i+j*n]*x[j];
-    }
+
+    free(y_local);
   }
 }
 
 void mat_vec_col_row_decomp(double* a, double* x, double* y, int n)
 {
-  
-  int i,j;
-
-  for(i=0;i<n;i++) y[i]=0.0;
-  for(j=0;j<n;j++){
-    for(i=0;i<n;i++){
+  for(int i=0;i<n;i++) y[i]=0.0;
+  for(int j=0;j<n;j++){
+    #pragma omp parallel for default(none) shared(a, x, y, n, j)
+    for(int i=0;i<n;i++){
       y[i]+=a[i+j*n]*x[j];
     }
   } 
