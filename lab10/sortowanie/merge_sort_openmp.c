d8c76c9a518f48e29bda9ef155182896e1cdfbcf
Twarug <patryk@twardosz.dev>
2024-12-08 19:03:44 +0100

    Lab10: Sortowanie (4.0)

--- lab10/sortowanie/merge_sort_openmp.c
+++ lab10/sortowanie/merge_sort_openmp.c
@@ -61,21 +61,27 @@ void merge_sort_openmp_2(
     int q1=(p+r)/2;
     
     // modyfikacja 1 - uzycie klauzuli final tak jak w merge_sort_openmp()
-#pragma omp task default(none) firstprivate(A,p,r,q1,poziom) 
+    #pragma omp task default(none) firstprivate(A,p,r,q1,poziom) final(poziom > max_poziom)
     {
       // modyfikacja 2 - w przypadku kiedy obowiązuje warunek z final (sprawdzenie
       // funkcją omp_in_final() ) uruchomienie funkcji sortowania szybkiego
       // z biblioteki sortowanie_seq: sortowanie_szybkie(A,p,q1);
-      merge_sort_openmp_2(A,p,q1,poziom);
+      if(omp_in_final())
+        sortowanie_szybkie(A, p, q1);
+      else
+        merge_sort_openmp_2(A,p,q1,poziom);
     }
 
     // modyfikacja 1 - uzycie klauzuli final tak jak w merge_sort_openmp()
-#pragma omp task default(none) firstprivate(A,p,r,q1,poziom) 
+    #pragma omp task default(none) firstprivate(A,p,r,q1,poziom) final(poziom > max_poziom)
     {
       // modyfikacja 2 - w przypadku kiedy obowiązuje warunek z final (sprawdzenie
       // funkcją omp_in_final() ) uruchomienie funkcji sortowania szybkiego
       // z biblioteki sortowanie_seq: sortowanie_szybkie(A,q1+1,r);
-      merge_sort_openmp_2(A,q1+1,r,poziom);
+      if(omp_in_final()) 
+        sortowanie_szybkie(A, q1 + 1, r);
+      else 
+        merge_sort_openmp_2(A,q1+1,r,poziom);
     }
 
 #pragma omp taskwait  
